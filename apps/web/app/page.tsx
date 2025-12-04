2e5fc576ad3e629c024c6d881ff8181be774bf25
dev-kishor <kishornathgupta1999@gmail.com>
2025-12-04 18:10:54 +0530

    chore: add pnpm workspace configuration and turbo.json for task management

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     <div aria-label="homeCard" className="flex justify-center items-center min-h-screen">
       <div className="p-8 rounded-lg text-center shadow-md ">
         <h2>Welcome to MatchChat!</h2>
-        <p>Connect with people around the world in real-time.</p>
+        <p>Connect with people around the world in real-time with 0 personal data shared.</p>
         <button type="button" className="text-white bg-blue-600 box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-full text-sm px-4 py-2.5 focus:outline-none ">Start chat</button>
       </div>
     </div>
